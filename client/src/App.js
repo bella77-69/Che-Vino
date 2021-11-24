3c5a01f4361a30877694fc5681cedae25cf1c0f5
chantelle mckenzie <mrspasceri@hotmail.com>
2021-11-24 13:43:08 -0800

    set up web application

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import Header from './Components/Header/Header';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+  <Header />
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+PORT = 8080;
+
+
+app.listen(8080, () => {
+    console.log(`listening on port ${PORT}...`);
+  });
