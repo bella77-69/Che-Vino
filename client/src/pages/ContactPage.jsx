8c095e5f22eee98bbb9a2f65f75420a938a407a6
chantelle mckenzie <mrspasceri@hotmail.com>
2021-11-28 21:57:14 -0800

    test

--- client/src/pages/ContactPage.jsx
+++ client/src/pages/ContactPage.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import './ContactPage.scss';
 
-
-
 export default function ContactPage() {
   const [inputs, setInputs] = useState({});
 
@@ -53,93 +51,5 @@ export default function ContactPage() {
   )
 }
 
-
-
-
-
- 
-
-
-//   render() {
-
-
-//     return (
-//       <form className="contact" onSubmit={this.validateForm} onChange={this.handleChange}>
-//         <div className="contact-container">
-//           <input clas="form-input" name="name" placeholder="Name" value={this.state.name} onChange={this.handleChange}/>
-//           {/* <input className="form-input" name="lastName" placeholder="Last Name" value={this.state.lastName} onChange={this.handleInputChange} /> */}
-//           <input className="form-input" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange}/>
-         
-//         </div>
-// <button className="contact-btn" value="submit" onClick={this.handleSumbit}>Submit</button>
-//       </form>
-//       // <div className="ContactView">
-//       //   <h1 className="ContactView-title">Contacts</h1>
-//       //   <ul className="ContactView-list">
-//       //     {/* {contacts.map(contact => <Contact key={contact.name} {...contact} />)} */}
-//       //   </ul>
-//       //   <form className="ContactForm">
-//       //     <input onChange={this.handleChange} name="name" type="text" value={name} placeholder="Name (required)" />
-//       //     <input onChange={this.handleChange} name="email" type="email" value={email} placeholder="Email (required)" />
-//       //     <textarea onChange={this.handleChange} name="description" value={description} placeholder="Enter a description" />
-//       //     <button type="button" onClick={this.handleSubmit}>Add Contact</button>
-//       //   </form>
-//       // </div>
-//     );
-//   }
-// }
-  // state={
-  //   id: " ",
-  //   wine: " ",
-  //   winery: " ",
-  //   location: "",
-  //   rating: " ",
-  //     average: " ",
-  //     reviews: " ",
-  //   };
-  
-  //   componentDidMount() {
-  //     this.getItem(this.props.match.params.id);
-  //   }
-
-  //   getItem = (id) => {
-  //     axios
-  //       .get(`http://localhost:8080/wines/rose/${id}`)
-  //       .then((response) =>
-  //         this.setState({
-  //           id: response.data.id,
-  //           wines: response.data.wine,
-  //           winery: response.data.winery,
-  //           location: response.data.location,
-  //           rating: response.data.rating,
-  //           reviews: response.data.reviews,
-  //           average: response.data.average,
-  //         })
-  //       )
-  //       .catch((err) => console.error(err));
-  //   };
-
-  
-      
- 
-      
-  //   render() {
-  //     const { wines, winery, location, rating, reviews, average} = this.state;
-  //     return (
-  //       <div className="contact">
-  //       <div className="contact-container">
-  //       <div className="item-details__container">
-  //         <div className="item-details__description-category-container">
-  //           <label className="item-details__label">Item description:</label>
-  //           <p className="item-details__info">{wines}</p>
-  //           <label className="item-details__label">Category:</label>
-  //           <p className="item-details__info">{winery}</p>
-  //         </div>
-  //     </div>
-  //       </div>
-  //       </div>
-  //     )
-  //   }
-  // }
   
 
