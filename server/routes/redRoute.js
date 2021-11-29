460b1e84e11cc8680dcbeb1aaad913b26e31f11f
chantelle mckenzie <mrspasceri@hotmail.com>
2021-11-29 09:28:01 -0800

    resolved merge conflicts

--- server/routes/redRoute.js
+++ server/routes/redRoute.js
@@ -16,29 +16,6 @@ function getRedsById(id) {
 router.get("/", (req, res) => {
     const reds = listReds();
     res.status(200).json(reds);
-<<<<<<< HEAD
-})
-
-
-router
-.get('/reds/:id', (req, res) => {
-    console.log(req.params.id);
-    fs.readFile('./data/red.json', 'utf-8', (err, data) => {
-        if (err) {
-            console.log(err);
-            res.json({message: 'error getting red wine id data'});
-        }
-        const redData = JSON.parse(data);
-        const foundRed = redData.find((red) => red.id == req.params.id);
-        if(!foundRed) {
-            res.status(404).send({message: 'No wine found with the id'});
-        } else {
-            res.json(foundRed);
-        }
-    });
-}
-)
-=======
 });
 router
 .get('/reds/:id', (req, res) => {
@@ -60,6 +37,5 @@ router
 )
 
 
->>>>>>> main
 
 module.exports = router;
